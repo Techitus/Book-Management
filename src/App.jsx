b6b484c0d4fdd15243ac2689c8bbc1793c874f1c
Kamal Paudel <techitus07@gmail.com>
2024-07-27 22:11:39 +0545

    implement lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
-import Login from './Pages/auth/Login'
-import Register from './Pages/auth/Register'
-import store from '../store/store'
-import { Provider } from 'react-redux'
-import Home from './Pages/blog/Home'
-import AddBlog from './Pages/blog/AddBlog'
-import SingleBlog from './Pages/blog/SingleBlog'
-import EditBlog from './Pages/blog/EditBlog'
-import Protected from './hof/Protected'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import './App.css';
+import Login from './Pages/auth/Login';
+import Register from './Pages/auth/Register';
+import store from '../store/store';
+import { Provider } from 'react-redux';
+import Home from './Pages/blog/Home';
+const AddBlog = lazy(() => import('./Pages/blog/AddBlog'));
+const SingleBlog = lazy(() => import('./Pages/blog/SingleBlog'));
+const EditBlog = lazy(() => import('./Pages/blog/EditBlog'));
+
+import Protected from './hof/Protected';
+import Spinner from './components/Spinner';
 
 function App() {
-  
-  
   return (
-    <>
     <Provider store={store}>
-    <BrowserRouter >
-    <Routes>
-      <Route path = '/login' element={<Login/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/' element={<Home/>}/>
-      <Route path = '/blog/add' element={<Protected><AddBlog/></Protected>}/>
-      <Route path = '/blog/:id' element={<Protected><SingleBlog/></Protected>}/>
-      <Route path = '/blog/edit/:id' element={<Protected><EditBlog/></Protected>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/blog/add'
+            element={
+              <Suspense fallback={<div className='text-white '><Spinner/></div>}>
+                <Protected>
+                  <AddBlog />
+                </Protected>
+              </Suspense>
+            }
+          />
+          <Route
+            path='/blog/:id'
+            element={
+              <Suspense fallback={<div><Spinner/></div>}>
+                <Protected>
+                  <SingleBlog />
+                </Protected>
+              </Suspense>
+            }
+          />
+          <Route
+            path='/blog/edit/:id'
+            element={
+              <Suspense fallback={<div><Spinner/></div>}>
+                <Protected>
+                  <EditBlog />
+                </Protected>
+              </Suspense>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
